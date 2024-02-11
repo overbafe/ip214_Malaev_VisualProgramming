cd5e975b0fc8c3df6309e13202cfd4e4733b27ca
Felix <malaevfelixf1234@gmail.com>
2024-02-11 23:34:56 +0700

    lab2

---
+++ lab2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace lab2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
